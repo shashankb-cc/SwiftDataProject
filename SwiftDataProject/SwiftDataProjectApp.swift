8f741fe71ca7b3846d20c9fc986accf76d5e33da
shashankb-cc <shashank.b@codecraft.co.in>
2025-02-25 12:54:17 +0530

    Initial Commit

---
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Shashank B on 25/02/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
