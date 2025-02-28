884ecd86c3f18e15d1a0f2bf7c65bbe34fa71d32
shashankb-cc <shashank.b@codecraft.co.in>
2025-02-28 11:37:44 +0530

    defined a relationship between user and job structs

--- SwiftDataProject/ContentView.swift
+++ SwiftDataProject/ContentView.swift
@@ -30,10 +30,11 @@ struct ContentView: View {
 //    }, sort: \User.name) var users: [User]
     @State private var path = [User]()
     @State private var showingUpcomingOnly = false
+    @State private var sortOrder = [SortDescriptor(\User.name),SortDescriptor(\User.joinDate)]
 
     var body: some View {
         NavigationStack (path: $path){
-            UsersView(minimumJoinDate: showingUpcomingOnly ? .now : .distantPast)
+            UsersView(minimumJoinDate: showingUpcomingOnly ? .now : .distantPast, sortOrder: sortOrder)
             .navigationTitle("Users")
             .navigationDestination(for: User.self) { user in
                 EditUserView(user: user)
@@ -43,7 +44,7 @@ struct ContentView: View {
                     
                     //tells SwiftData to tell all existing model objects of the the type User, which means the database is clear before we add the sample users.
                     try? modelContext.delete(model: User.self)
-
+                    
                     let first = User(name: "Ed Sheeran", city: "London", joinDate: .now.addingTimeInterval(86400 * -10))
                     let second = User(name: "Rosa Diaz", city: "New York", joinDate: .now.addingTimeInterval(86400 * -5))
                     let third = User(name: "Roy Kent", city: "London", joinDate: .now.addingTimeInterval(86400 * 5))
@@ -57,6 +58,36 @@ struct ContentView: View {
                 
                 Button(showingUpcomingOnly ? "Show Everyone" : "Show Upcoming") {
                     showingUpcomingOnly.toggle()
+                }
+                
+                //                Picker("Sort", selection: $sortOrder) {
+                //                    Text("Sort by Name")
+                //                        .tag([
+                //                            SortDescriptor(\User.name),
+                //                            SortDescriptor(\User.joinDate),
+                //                        ])
+                //
+                //                    Text("Sort by Join Date")
+                //                        .tag([
+                //                            SortDescriptor(\User.joinDate),
+                //                            SortDescriptor(\User.name)
+                //                        ])
+                //                }
+                Menu("Sort", systemImage: "arrow.up.arrow.down") {
+                    
+                    Picker("Sort", selection: $sortOrder) {
+                        Text("Sort by Name")
+                            .tag([
+                                SortDescriptor(\User.name),
+                                SortDescriptor(\User.joinDate),
+                            ])
+                        
+                        Text("Sort by Join Date")
+                            .tag([
+                                SortDescriptor(\User.joinDate),
+                                SortDescriptor(\User.name)
+                            ])
+                    }
                 }
             }
             
--- SwiftDataProject/Models/User.swift
+++ SwiftDataProject/Models/User.swift
@@ -8,12 +8,29 @@
 import Foundation
 import SwiftData
 
+
+@Model
+class Job {
+    var name: String
+    var priority: Int
+    var owner: User?
+
+    init(name: String, priority: Int, owner: User? = nil) {
+        self.name = name
+        self.priority = priority
+        self.owner = owner
+    }
+}
+
 @Model
 class User {
     var name:String
     var city:String
     var joinDate:Date
     
+    //this is similar to on delete cascase of mysql else only user wil be set to null
+    @Relationship(deleteRule: .cascade) var jobs = [Job]()
+
     init(name: String, city: String, joinDate: Date) {
         self.name = name
         self.city = city
